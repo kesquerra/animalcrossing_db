8a09a16498c4b560f2d77586d4efc8fe1b7144b5
kesquerra <esquerrk@oregonstate.edu>
2020-10-29 13:05:53 -0700

    add page shells

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,8 +6,38 @@ const router = express.Router();
 // home page route
 router.get("/", (req, res, next) => {
   res.status(200).render("home", {
-      css: ["home.css"]
+    css: ["home.css"]
   });
 });
+
+router.get("/villager", (req, res, next) => {
+    res.status(200).render("villager", {
+      css: ["table.css"]
+    });
+});
+
+router.get("/personality", (req, res, next) => {
+    res.status(200).render("personality", {
+      css: ["table.css"]
+    });
+});
+
+router.get("/species", (req, res, next) => {
+    res.status(200).render("species", {
+      css: ["table.css"]
+    });
+});
+
+router.get("/island", (req, res, next) => {
+    res.status(200).render("island", {
+      css: ["table.css"]
+    });
+});
+
+router.get("/facility", (req, res, next) => {
+    res.status(200).render("facility", {
+      css: ["table.css"]
+    });
+  });
 
 module.exports = router;
