1fc23cef11e92ef7321f023b78ee35eb3b864b0c
mareel <mareel@oregonstate.edu>
2020-11-08 17:16:48 -1000

    Event listeners to home buttons

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -2,6 +2,21 @@
 
 document.getElementById("island").addEventListener("click", function() {
     console.log('hi');
+    $('#add-modal').modal('show');
 });
 
-console.log("hello");
+document.getElementById("villager").addEventListener("click", function() {
+    $('#add-modal').modal('show');
+});
+
+document.getElementById("personality").addEventListener("click", function() {
+    $('#add-modal').modal('show');
+});
+
+document.getElementById("species").addEventListener("click", function() {
+    $('#add-modal').modal('show');
+});
+
+document.getElementById("facility").addEventListener("click", function() {
+    $('#add-modal').modal('show');
+});
