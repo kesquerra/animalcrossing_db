cb26cba71570b62b5a5ed1713c5c316da3983284
heinl11 <Winney_AM_1522>
2020-11-27 15:50:44 -1000

    ajax to put route from update form updated

--- static/js/update_button.js
+++ static/js/update_button.js
@@ -1,6 +1,5 @@
 var table = document.getElementsByTagName("table")[0];
 var tbody = table.getElementsByTagName("tbody")[0];
-var modal = document.getElementById("update-modal");
 
 table.addEventListener('click', (updateButton));
 
@@ -25,47 +24,57 @@ function updateButton(event) {
 };
 
 function createModalData(data) {
+    modalData = data.slice(0, );
     if (table.id == "species" || table.id == "island") {
-        column_names = ["name"];
+        column_names = [table.id + "ID", "name"];
     }
+    console.log(table.id + "ID")
     if (table.id == "facility") {
-        column_names = ["name", "available"];
+        column_names = ["facilityID", "name", "available"];
     }
     if (table.id == "villager") {
-        column_names = ["name", "birthday", "hobby", "image_url", "species", "personality"];
-        var dates = new Date(data[2]);
+        column_names = ["villagerID", "name", "birthday", "hobby", "image_url", "species", "personality"];
+        var dates = new Date(modalData[2]);
         var new_date = dates.toISOString();
         new_date = new_date.slice(0, 10);
-        data[2] = new_date;
+        modalData[2] = new_date;
     }
     if (table.id == "personality") {
-        column_names = ["name", "description", "wakeTime", "sleepTime", "activities"]
+        column_names = ["personalityID", "name", "description", "wakeTime", "sleepTime", "activities"]
     }
     if (table.id == "island_villager") {
         column_names = ["islandID", "villagerID"]
-        modalData = data.slice(0, );
     } 
     if (table.id == "island_facility") {
         column_names = ["islandID", "facilityID"]
-        modalData = data.slice(0, );
+
     }
     if (table.id == "compatibility") {
         column_names = ["p1", "p2"]
-        modalData = data.slice(0, );
     } 
-    if (column_names.length < data.length) {
-        modalData = data.slice(1, );
-    }
-    console.log(modalData)
     openModal(modalData, column_names)
 }
 
 function openModal(modalData, column_names) {
     $(window).on('shown.bs.modal', function() { 
+        document.getElementById("form1").action = "/" + table.id + "/update";
         $('#update-modal').modal('show');
+        submit = document.getElementById("submit1")
+        submit.addEventListener('click', (updateData))
         for (i = 0; i < modalData.length; i++) {
-            document.getElementById(column_names[i]).value = modalData[i]
+            document.getElementById(column_names[i]).value = modalData[i];
         }
     });
     $("#update-modal").modal();
+}
+
+function updateData() {
+    $.ajax({
+        url: '/' + table.id + '/update', 
+        type: "PUT", 
+        data: $('#form1').serialize(), 
+        success: function(result) {
+            window.location.replace('../')
+        }
+    })
 }
